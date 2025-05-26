de67c61f13a0b6e2cec65ce100a85c3a3f47f00a
Matysse <mathis.jung@blgcloud.com>
2025-05-26 14:27:58 +0200

    initial message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,33 @@
 <script>
-  const email = "tonadresse@email.com";
+  const email = "maellecontrevent@gmail.com";
 </script>
 
-<main class="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-sky-100 via-gray-200 to-gray-300 text-gray-800 font-serif p-8">
-  <div class="max-w-xl text-center space-y-6">
+<main class="relative h-screen md:min-h-screen md:flex md:flex-col items-center justify-center bg-gradient-to-b from-sky-100 via-gray-200 to-gray-300 text-gray-800 font-serif md:p-8 overflow-hidden">
+  <!-- Contenu principal -->
+  <div class="relative h-full md:h-fit z-10 md:max-w-2/3 text-center flex flex-col justify-center items-center gap-6 md:bg-white/70 backdrop-blur-sm p-4 md:p-6 md:rounded-2xl shadow-md">
     <h1 class="text-4xl md:text-5xl font-bold tracking-wide">
-      "Nous allons contre le vent..."
+      "Contre le vent, mais pas contre les hommes."
     </h1>
-    <p class="text-lg md:text-xl italic text-gray-600">
-      Tu as suivi les traces des derniers traceurs.<br />
-      Si tu cherches à me joindre dans ce souffle permanent, écris-moi.
+
+    <p class="text-lg italic text-gray-600">
+      Ce site n’est pas officiel.
     </p>
+
+    <p class="text-md text-gray-700 leading-relaxed">
+      Lectrice admirative de <em>La Horde du Contrevent</em>, j’ai récupéré ce domaine, inactif depuis un moment.
+      Je n’ai jamais pu découvrir la <strong>bande défilée</strong> disparue, mais son souvenir m’intrigue.
+    </p>
+
+    <p class="text-md text-gray-700 leading-relaxed">
+      Je ne détiens aucun droit sur l’œuvre. Si vous êtes ayant droit ou concerné·e, n'hésitez pas à me contacter !
+    </p>
+
     <a 
       href="mailto:{email}" 
-      class="inline-block mt-4 px-6 py-3 border border-gray-700 text-gray-700 hover:bg-gray-700 hover:text-white transition rounded-xl shadow-md"
+      class="inline-block mt-4 w-fit px-6 py-3 border border-gray-700 text-gray-700 hover:bg-gray-700 hover:text-white transition rounded-xl shadow-md"
     >
       Me contacter
     </a>
+    <em>maellecontrevent@gmail.com</em>
   </div>
 </main>
